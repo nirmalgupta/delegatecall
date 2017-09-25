b03970f0843c28e98c3deca70c51f382c75893b6
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-09-24 22:18:46 -0700

    initial update.

--- test/custody.js
+++ test/custody.js
@@ -9,14 +9,12 @@ contract('deposit', function (accounts) {
 
   before(async function () {
     custody = await Custody.deployed();
-    console.log('custody', custody.address);
     token = await StandardToken.new(1000000, "some awesome token", 0, "SAT");
     await token.transfer(accounts[1], 10000);
     await custody.setToken(token.address);
   });
 
   it('user is able to send tokens to custody contract', async function () {
-    console.log('custody', custody.address);
     await custody.delegateTokens(100, {from: user1});
     expect((await custody.tokens(user1)).toNumber()).to.eql(100);
     expect((await token.balanceOf(user1)).toNumber()).to.be.eql(9900);
